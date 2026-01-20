03f1ae319feb9da0e7eb380137c30b512c46b778
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-01-21 03:30:13 +0800

    fix localpath getter

--- Aurorae/Utils/LocalPath.cs
+++ Aurorae/Utils/LocalPath.cs
@@ -2,6 +2,6 @@ namespace Aurorae.Utils;
 
 public static class LocalPath
 {
-    public static string Gallery => Environment.GetEnvironmentVariable("AURORAE_GALLERY") ?? Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-    public static string HttpProxy => Environment.GetEnvironmentVariable("AURORAE_HTTP_PROXY")!;
+    public static string Gallery { get; } = Environment.GetEnvironmentVariable("AURORAE_GALLERY") ?? Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+    public static string HttpProxy { get; } = Environment.GetEnvironmentVariable("AURORAE_HTTP_PROXY")!;
 }
