e65ade0cf9b7586283de866c99aaf3028a733579
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-01-28 17:43:23 +0800

    doc path

--- Aurorae/Utils/LocalPath.cs
+++ Aurorae/Utils/LocalPath.cs
@@ -3,5 +3,6 @@ namespace Aurorae.Utils;
 public static class LocalPath
 {
     public static string Gallery { get; } = Environment.GetEnvironmentVariable("AURORAE_GALLERY") ?? Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+    public static string Document { get; } = Environment.GetEnvironmentVariable("AURORAE_DOCUMENT") ?? Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
     public static string HttpProxy { get; } = Environment.GetEnvironmentVariable("AURORAE_HTTP_PROXY")!;
 }
