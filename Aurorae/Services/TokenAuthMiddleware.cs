68ca4fb0283ffebc97506492774423a6163a6828
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-02-24 19:31:02 +0800

    cache is evil

--- Aurorae/Services/TokenAuthMiddleware.cs
+++ Aurorae/Services/TokenAuthMiddleware.cs
@@ -4,7 +4,6 @@ namespace Aurorae.Services;
 
 public class TokenAuthMiddleware(AuroraeDb db) : IMiddleware
 {
-    private static HashSet<string> tokens = [];
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         if (context.Request.Path.StartsWithSegments("/auth"))
@@ -13,20 +12,8 @@ public class TokenAuthMiddleware(AuroraeDb db) : IMiddleware
             return;
         }
 
-        if (!context.Request.Cookies.TryGetValue("access_token", out var token))
-        {
-            context.Abort();
-            return;
-        }
-
-        static bool IsValid(string token) => tokens.Contains(token);
-
-        if (!IsValid(token))
-        {
-            tokens = await db.AccessTokens.Select(x => x.Token).ToHashSetAsync();
-        }
-
-        if (!IsValid(token))
+        if (!context.Request.Cookies.TryGetValue("access_token", out var token) ||
+            !await db.AccessTokens.AnyAsync(x => x.Token == token))
         {
             context.Abort();
             return;
